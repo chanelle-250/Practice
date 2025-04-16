d8a79b7a337ad5370b86b428c35e66532a5fa6c4
Chanelle-001 <chanelleingabire01@gmail.com>
2025-04-16 16:19:52 +0200

    add StudentManager and Main

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Student {
+    public String fname;
+    public int marks;
+
+    public Student(String fname, int marks) {
+        this.fname = fname;
+        this.marks = marks;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+    public int getMarks() {
+        return marks;
+    }
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String toString() {
+        return "Student [fname=" + fname + ", marks=" + marks + "]";
+
+    }
 }
--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import javax.imageio.IIOException;
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
+
 
 public class WriteToFile {
+    public static void main(String[] args) {
+        List <Student>students = new ArrayList<Student>();
+
+        students.add(new Student("chanelle",90));
+        students.add(new Student("david",90));
+
+        String filename = "C:\\Users\\irako\\IdeaProjects\\practice\\src\\trial.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            for (Student score : students) {
+                System.out.println(students.indexOf(score));
+                bw.write(score.toString());
+                bw.newLine();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
+
 }
